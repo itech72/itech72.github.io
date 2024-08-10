fc678d57188c30ad479070f505046c6c7afb7462
itech72 <92259074+itech72@users.noreply.github.com>
2024-08-10 23:22:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,88 @@
-//Global variables
-const MathOperators = ["+", "-", "*", "/"];
+/////////////////////////////////////////
+//////////////// Swipe Logic ////////////
+/////////////////////////////////////////
 
+let container1 = document.getElementById("history");
+let container2 = document.getElementById("display");
+let container3 = document.getElementById("display2");
+
+let containers = [container1, container2, container3];
+
+// Define variables for startX, diffX
+let startX = 0;
+let diffX = 0;
+
+// Define the elements you want to manipulate
+let mode1 = document.getElementById("mode1");
+let mode2 = document.getElementById("mode2");
+
+// Function to handle touch start
+function handleTouchStart(e) {
+    startX = e.touches[0].clientX;
+}
+
+// Function to handle touch end
+function handleTouchEnd(e) {
+    let endX = e.changedTouches[0].clientX;
+    diffX = endX - startX;
+
+    if (diffX > 50) {
+        // Right swipe
+        mode2.classList.add("slide-right");
+        mode2.classList.remove("slide-middle");
+        mode1.classList.add("slide-middle");
+        mode1.classList.remove("slide-left");
+    } else if (diffX < -50) {
+        // Left swipe
+        Display2.value = parseFloat(totalVolume.toFixed(3));
+        mode1.classList.add("slide-left");
+        mode1.classList.remove("slide-middle");
+        mode2.classList.add("slide-middle");
+        mode2.classList.remove("slide-right");
+    }
+}
+
+// Attach event listeners to each container
+containers.forEach(container => {
+    container.addEventListener("touchstart", handleTouchStart);
+    container.addEventListener("touchend", handleTouchEnd);
+});
+
+
+/////////////////////////////////////////
+//////////////// Mode 1 /////////////////
+/////////////////////////////////////////
+
+//Global variables
 let IsReset = true;
 let CheckSymbol = true;
 let totalPieces = 0;
 let totalVolume = 0;
 let Display = document.getElementById("display");
 let history = document.getElementById("history");
+
+const buttons = document.querySelectorAll("#keypad button");
+
+function triggerVibration() {
+  if (navigator.vibrate) {
+    navigator.vibrate(50); // Vibrate for 50ms
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("touchstart", function () {
+    button.classList.add("touch-active");
+    triggerVibration();
+  });
+
+  button.addEventListener("touchend", function () {
+    button.classList.remove("touch-active");
+  });
+
+  button.addEventListener("touchcancel", function () {
+    button.classList.remove("touch-active");
+  });
+});
 
 Display.addEventListener("input", (e) => {
   validateState();
@@ -39,20 +115,6 @@ function display(num) {
   scrollDisplayToRight();
 }
 
-function calculate() {
-  try {
-    compute = Display.value;
-    let result = eval(compute);
-    let roundResult = Math.round(result * 100000000) / 100000000; // 8 percision
-    Display.value = roundResult;
-    validateState();
-  } catch (err) {
-    alert(err);
-  }
-
-  scrollDisplayToBeginning();
-}
-
 function reset() {
   Display.value = 0;
   CheckSymbol = true;
@@ -78,11 +140,6 @@ function getLastCharacter(text) {
   return text.slice(-1);
 }
 
-function getLastMathOperatorIndex(text) {
-  const regex = /(?:\+|\-|\*|\/)(?!.*(?:\+|\-|\*|\/))/;
-  return regex.exec(text)?.index;
-}
-
 function scrollDisplayToRight() {
   Display.scrollLeft = Display.scrollWidth;
 }
@@ -96,26 +153,6 @@ function scrollDisplayToBeginning() {
   Display.scrollLeft = 0;
 }
 
-function setCharAt(str, index, chr) {
-  if (index > str.length - 1) {
-    return str;
-  }
-
-  return str.substring(0, index) + chr + str.substring(index + 1);
-}
-
-function addCharAt(str, index, chr) {
-  if (index > str.length - 1) {
-    return str;
-  }
-
-  let a = str.slice(0, index);
-  let b = chr;
-  let c = str.slice(index, str.length);
-
-  return a + b + c;
-}
-
 function formatNumber() {
   var input = Display.value;
   var outputList = document.getElementById("historyList");
@@ -127,7 +164,7 @@ function formatNumber() {
   var vol = (firstSlice * firstSlice * secondSlice) / 2304;
 
   var div1 = document.createElement("div");
-  div1.textContent = input.slice(0, -1) + "' x " + input.slice(-1) + "'";
+  div1.textContent = input.slice(0, -1) + '" x ' + input.slice(-1) + "'";
   div1.classList.add("history-item");
   listItem.appendChild(div1);
 
@@ -167,17 +204,124 @@ function updateTotal(totalPieces, totalVolume) {
   totalVolumeElement.textContent = totalVolume.toFixed(3) + " cft";
 }
 
-if (navigator.serviceWorker) {
-  // Start registration process on every page load
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      // The register function takes as argument
-      // the file path to the worker's file
-      .register("./sw.js")
-      // Gives us registration object
-      .then((reg) => console.log("Service Worker Registered"))
-      .catch((swErr) =>
-        console.log(`Service Worker Installation Error: ${swErr}}`)
-      );
+/////////////////////////////////////////
+//////////////// Mode 2 /////////////////
+/////////////////////////////////////////
+
+//Global variables
+const MathOperators = ["+", "-", "*", "/"];
+
+let IsReset2 = true;
+let CheckSymbol2 = true;
+let Display2 = document.getElementById("display2");
+
+const buttons2 = document.querySelectorAll("#keypad2 button");
+
+buttons2.forEach((button) => {
+  button.addEventListener("touchstart", function () {
+    button.classList.add("touch-active");
+    triggerVibration();
   });
+
+  button.addEventListener("touchend", function () {
+    button.classList.remove("touch-active");
+  });
+
+  button.addEventListener("touchcancel", function () {
+    button.classList.remove("touch-active");
+  });
+});
+
+Display2.addEventListener("input", (e) => {
+  validateState2();
+  scrollDisplayToRight2();
+
+  if (IsReset2) {
+    let newValue = e.target.value;
+    Display2.value = newValue.substring(1);
+    IsReset2 = false;
+  }
+});
+
+// Functions Declarations
+function validateState2() {
+  if (Display2.value === "") {
+    Display2.value = 0;
+    IsReset2 = true;
+    console.log(`${IsReset2}`);
+  }
+
+  let lastCharacter = getLastCharacter(Display2.value);
+  CheckSymbol2 = !MathOperators.includes(lastCharacter);
+}
+
+function display2(num) {
+  let shouldReplaceNumberZero = Display2.value === "0";
+  Display2.value = shouldReplaceNumberZero ? num : Display2.value + num;
+  CheckSymbol2 = true;
+  IsReset2 = false;
+  scrollDisplayToRight2();
+}
+
+function calculate() {
+  try {
+    compute = Display2.value;
+    let result = eval(compute);
+    let roundResult = Math.round(result * 100000000) / 100000000; // 8 percision
+    Display2.value = parseFloat(roundResult.toFixed(2));
+    validateState2();
+  } catch (err) {
+    alert(err);
+  }
+
+  scrollDisplayToBeginning2();
+}
+
+function reset2() {
+  Display2.value = 0;
+  CheckSymbol2 = true;
+  IsReset2 = true;
+}
+
+function removeCharacter2() {
+  const endIndex = Display2.value.length - 1;
+  Display2.value = Display2.value.slice(0, endIndex);
+  validateState2();
+}
+
+function getLastCharacter(text) {
+  return text.slice(-1);
+}
+
+function getLastMathOperatorIndex(text) {
+  const regex = /(?:\+|\-|\*|\/)(?!.*(?:\+|\-|\*|\/))/;
+  return regex.exec(text)?.index;
+}
+
+function scrollDisplayToRight2() {
+  Display2.scrollLeft = Display2.scrollWidth;
+}
+
+function scrollDisplayToBeginning2() {
+  Display2.scrollLeft = 0;
+}
+
+function setCharAt(str, index, chr) {
+  if (index > str.length - 1) {
+    return str;
+  }
+
+  return str.substring(0, index) + chr + str.substring(index + 1);
+}
+
+function addCharAt(str, index, chr) {
+  if (index > str.length - 1) {
+    return str;
+  }
+
+  let a = str.slice(0, index);
+  let b = chr;
+  let c = str.slice(index, str.length);
+
+  return a + b + c;
 }
